87a2340bf38ae1ed6ae982e460123e912f9199de
serezhaSM <ser.v.smirnov@rt.ru>
2022-04-07 13:32:05 +0000

    В settings сделал настройку для mysql

--- vims_audit/vims_audit/settings.py
+++ vims_audit/vims_audit/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'vims_audit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'vims_audit',
+        'USER': 'vims',
+        'PASSWORD': 'Jdthnfqv60!',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
